f61d68c2aaabeac1571f7b2e347793636f41f8e7
gcharland1 <g.charland1@protonmail.com>
2022-01-15 11:28:48 -0500

    Added CSS and first Angular logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Donne acces a ngModel
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
+  // Ce que j'ai déclaré dans les autres sous-modules
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  // Ce qu'il faut importer depuis les internets
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
+// L'équivalent de Implements / Override
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    if (this.selectedHero == hero) {
+      this.selectedHero = undefined;
+    } else {
+      this.selectedHero = hero;
+    }
+  }
 }
