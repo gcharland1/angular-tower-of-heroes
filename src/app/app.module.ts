cbb4f2f3f70fb6a29543596ecbc5eeca198d2501
gcharland1 <g.charland1@protonmail.com>
2022-01-16 14:44:49 -0500

    added remote server service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Donne acces a ngModel
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -23,7 +26,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false}
+    )
   ],
 
   providers: [],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -11,11 +12,13 @@ import { MessageService } from './message.service';
 
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
+
+  private heroesUrl = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    const heroes = this.http.get<Hero[]>(this.heroesUrl);
+    this.log(`fetched heroes from ${this.heroesUrl}`)
     return heroes;
   }
 
@@ -23,14 +26,14 @@ export class HeroService {
     const hero = HEROES.find(h => h.id == id)!;
     // Le ! à la fin renvoie <undefined> s'il n'y a pas de hero
     if (typeof hero == typeof new Observable<Hero>()) {
-      this.messageService.add(`HeroService: fetched hero id=${id}`);
+      this.log(`fetched hero id=${id}`);
     } else {
-      this.messageService.add(`No hero with id=${id}`);
+      this.log(`No hero with id=${id}`);
     }
     return of(hero);
   }
 
-  addMessage(message: string): void {
-    this.messageService.add(message);
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
